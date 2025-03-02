e2cc5ed446f92222ebf7a2ef2aef38dd83e4e6ee
sjtuross <sjtuross@msn.com>
2025-03-02 22:07:46 +0800

    optimize: intro skip for multi versions

--- StrmAssistant/IntroSkip/PlaySessionMonitor.cs
+++ StrmAssistant/IntroSkip/PlaySessionMonitor.cs
@@ -115,7 +115,11 @@ namespace StrmAssistant.IntroSkip
 
         private void OnPlaybackStart(object sender, PlaybackProgressEventArgs e)
         {
-            if (!(e.Item is Episode) || !e.PlaybackPositionTicks.HasValue) return;
+            if (!(e.Item is Episode episode) || !e.PlaybackPositionTicks.HasValue || !episode.IndexNumber.HasValue ||
+                !(episode.ParentIndexNumber > 0))
+            {
+                return;
+            }
 
             var options = Plugin.Instance.IntroSkipStore.GetOptions();
 
